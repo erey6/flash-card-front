d0e96246fce96dbab37c3c4ac1ec43cce287a269
Eric Reyes <ericreyes@gmail.com>
2021-12-08 15:26:23 -0600

    can add options

--- flashcards/src/pages/EditQuiz.js
+++ flashcards/src/pages/EditQuiz.js
@@ -1,13 +1,31 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useReducer } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "add":
+            const foundQuestion = action.updatedQQ.find((question) => question.id == action.id)
+            const foundIndex = action.updatedQQ.indexOf(foundQuestion)
+            foundQuestion.options.push('')
+            console.log(foundQuestion)
+            action.updatedQQ.splice(foundIndex, 1, foundQuestion)
+            return action.updatedQQ
+        case "subtract":
+            return state;
+        default:
+            return state;
+
+    }
+}
+
 
 const EditQuiz = (props) => {
     const emptyQuestion = {
         "id": 0,
         "query": "",
         "options": [
-            "",
+            "c",
             "",
             ""
         ],
@@ -17,7 +35,8 @@ const EditQuiz = (props) => {
     const [updatedQuiz, setUpdatedQuiz] = useState(props.currentQuiz)
     const [checkbox, setCheckbox] = useState(!props.currentQuiz.private)
     const [updatedQQ, setUpdatedQQ] = useState([emptyQuestion])
-    const [changingQuestion, setChangingQuestion] = useState({id:0})
+    const [changingQuestion, setChangingQuestion] = useState({ id: 0 })
+    const [allQuestions, dispatch] = useReducer(reducer, [{}])
 
     const navigate = useNavigate()
     const handleChange = (event) => {
@@ -33,12 +52,12 @@ const EditQuiz = (props) => {
         }
         newOptions.pop()
         newOptions.splice(0, 3)
-        setChangingQuestion({"id": e.target[1].value, "query": e.target[0].value, "options": newOptions, "quizId": e.target[2].value, "quiz": null })
+        setChangingQuestion({ "id": e.target[1].value, "query": e.target[0].value, "options": newOptions, "quizId": e.target[2].value, "quiz": null })
     }
 
     const handleCheck = () => {
         setCheckbox(!checkbox)
-        setUpdatedQuiz({ ...updatedQuiz, ["private"]: checkbox })
+        setUpdatedQuiz({ ...updatedQuiz, "private": checkbox })
     }
 
     const handleSubmit = (e) => {
@@ -55,30 +74,39 @@ const EditQuiz = (props) => {
 
     const postChange = () => {
         if (changingQuestion.id) {
-        axios
-            .put(`https://flashcard6.azurewebsites.net/api/Questions/${changingQuestion.id}`,
-                changingQuestion)
-            .then((response) => {
-                props.findUsersQuizzes(response.data)
-                //some kind of response?
-            })
+            axios
+                .put(`https://flashcard6.azurewebsites.net/api/Questions/${changingQuestion.id}`,
+                    changingQuestion)
+                .then((response) => {
+                    props.findUsersQuizzes(response.data)
+                    //some kind of response?
+                })
         }
     }
 
     const handleQuestionDelete = (e) => {
         props.deleteSomething(e.target.value, "Questions");
         navigate("/editquiz");
-    } 
+    }
 
     const handleQuizDelete = (e) => {
         props.deleteSomething(e.target.value, "Quizzes");
         navigate("/home");
-    } 
+    }
+
+    const handleAddQuestion = () => {
+        navigate("/addquestion")
+    }
+
+    const handleAddOptions = (e) => {
+       const id = e.currentTarget.value
+        dispatch({type: "add", updatedQQ, id})
+    }
 
     useEffect(() => {
         setUpdatedQQ(props.quizQuestions)
     }, [props.quizQuestions])
-    
+
     useEffect(() => {
         postChange()
     }, [changingQuestion])
@@ -97,22 +125,23 @@ const EditQuiz = (props) => {
                     <label htmlFor="private" className="ml-2">Share with public</label>
                 </div>
                 <button className="h-8" type="submit">Submit Name/Description Changes</button>
-                <button className="h-8 bg-red-800" value={updatedQuiz.id} onClick={handleQuizDelete}type="button">Delete quiz</button>
+                <button className="h-8 bg-red-800" value={updatedQuiz.id} onClick={handleQuizDelete} type="button">Delete quiz</button>
             </form>
-
+            <div className="w-2/3 items-baseline flex mt-6 justify-between"><h2 className="mt-9">Edit Questions</h2> <button type="button" onClick={handleAddQuestion}>Add a question</button></div>
 
             {updatedQQ.map((question, index) => {
                 return (
 
-                    <div key={question.id} className="p-4 my-6 w-2/3 bg-gray-300 rounded-sm border-2 border-gray-300">
+                    <div key={question.id} className="p-4 my-9 w-2/3 bg-gray-300 rounded-sm border-2 border-gray-300">
                         <form onSubmit={handleQuestionSubmit}>
                             <label htmlFor="query">Question</label>
+
                             <input className="w-full shadow resize-vertical border block rounded py-1 px-3 text-gray-700 my-2" type="text" name={`${index}`} defaultValue={question.query} autoComplete="off" />
                             <input type="hidden" value={question.id} />
                             <input type="hidden" value={question.quizId} />
-                            <label>Options</label>
+                            <div className="flex justify-between"><label>Options</label><button type="button" value={question.id} onClick={handleAddOptions}>Add option +</button></div>
                             {question.options.map((anOption, index) => (<input name={index} className="w-full" key={index} defaultValue={anOption} />))}
-                            <button className="h-8" type="submit">Submit changes to this question</button> <button className="h-8 bg-red-800" value={question.id} onClick={handleQuestionDelete}type="button">Delete question</button>
+                            <button className="h-8" type="submit">Submit changes to this question</button> <button className="h-8 bg-red-800" value={question.id} onClick={handleQuestionDelete} type="button">Delete question</button>
                         </form>
                     </div>
 
